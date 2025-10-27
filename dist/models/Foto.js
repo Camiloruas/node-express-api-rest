11abf7b0c468293cc8407602c83350b4084b8c1b
Camiloruas <miloruas@gmail.com>
2025-10-27 13:51:18 -0300

    Feito

--- dist/models/Foto.js
+++ dist/models/Foto.js
@@ -26,7 +26,7 @@ var _appConfig = require('../../config/appConfig'); var _appConfig2 = _interopRe
         url: {
           type: _sequelize2.default.VIRTUAL,
           get() {
-            return `${_appConfig2.default.url}/images/${this.getDataValue("filename")}`;
+            return new URL(`/images/${this.getDataValue("filename")}`, _appConfig2.default.url).href;
           },
         },
         // >>> ADIÇÃO CRÍTICA DO CAMPO aluno_id <<<
